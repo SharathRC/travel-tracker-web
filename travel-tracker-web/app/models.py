bf88291abb558e955a6ed8e4789516bee49df636
Sharath RC <sharathr.sh@gmail.com>
2023-10-22 21:23:58 +0200

    add trip model

--- travel-tracker-web/app/models.py
+++ travel-tracker-web/app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Trip(models.Model):
+    SCHEDULED = "scheduled"
+    ACTIVE = "active"
+    COMPLETED = "completed"
+    DELETED = "deleted"
+
+    STATUS_CHOICES = (
+        (SCHEDULED, "Scheduled"),
+        (ACTIVE, "Active"),
+        (COMPLETED, "Completed"),
+        (DELETED, "Deleted"),
+    )
+    user = models.ForeignKey(User, related_name="trips", on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default=ACTIVE)
+
+    class Meta:
+        verbose_name_plural = "Trips"
+        ordering = ("-created_at",)
+
+    def __str__(self) -> str:
+        return self.title
