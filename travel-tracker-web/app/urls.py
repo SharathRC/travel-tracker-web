4ca57bcafac277d0387657c814648f5417aaaf21
Sharath RC <sharathr.sh@gmail.com>
2023-10-22 22:27:37 +0200

    added trips details

--- travel-tracker-web/app/urls.py
+++ travel-tracker-web/app/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import trip_detail
 
-urlpatterns = []
+
+urlpatterns = [
+    path("trips/<slug:slug>/", trip_detail, name="trip_detail"),
+]
--- travel-tracker-web/app/views.py
+++ travel-tracker-web/app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from .models import Trip
+
+
+def trip_detail(request, slug):
+    trip = get_object_or_404(Trip, slug=slug, status=Trip.ACTIVE)
+    context = {"trip": trip}
+
+    return render(request, "trip_detail.html", context)
