080780172961395f8e57771c98312b653c87d314
Sharath RC <sharathr.sh@gmail.com>
2023-10-22 22:36:22 +0200

    connect trips to homepage

--- travel-tracker-web/core/views.py
+++ travel-tracker-web/core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from app.models import Trip
+
 
 def homepage(request):
-    context = {}
+    # products = Trip.objects.filter(status=Trip.ACTIVE)
+    trips = Trip.objects.all()
+    context = {"trips": trips}
 
     return render(request, "homepage.html", context)
