d0f30ca716f000f27ca861c2d183b9dead03bbe4
Sharath RC <sharathr.sh@gmail.com>
2023-10-31 18:24:09 +0530

    new new trip

--- travel-tracker-web/app/urls.py
+++ travel-tracker-web/app/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import trip_detail
+from .views import (
+    trip_detail,
+    add_new_trip,
+)
 
 
 urlpatterns = [
     path("trips/<slug:slug>/", trip_detail, name="trip_detail"),
+    path("add-new-trip/", add_new_trip, name="add_trip"),
 ]
--- travel-tracker-web/app/views.py
+++ travel-tracker-web/app/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils.text import slugify
+from django.contrib import messages
 
 from .models import Trip
+from .forms import TripForm
 
 
 def trip_detail(request, slug):
-    trip = get_object_or_404(Trip, slug=slug, status=Trip.ACTIVE)
+    trip = get_object_or_404(Trip, slug=slug)
     context = {"trip": trip}
 
     return render(request, "trip_detail.html", context)
+
+
+def add_new_trip(request):
+    if request.method == "POST":
+        form = TripForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            title = request.POST.get("title")
+
+            trip = form.save(commit=False)
+            trip.user = request.user
+
+            slug = f"{trip.user}-{title}"
+            trip.slug = slugify(slug)
+            trip.save()
+
+            messages.success(request, "Trip added successfully!")
+
+            return redirect("homepage")
+    else:
+        form = TripForm()
+
+    return render(request, "add_trip.html", {"form": form})
