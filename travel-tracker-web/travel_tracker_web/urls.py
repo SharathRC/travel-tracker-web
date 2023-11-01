cce4eb62d416f74d985b98d5cd263e2af4278ae5
Sharath RC <sharathr.sh@gmail.com>
2023-11-02 00:21:24 +0530

    fix media missing links

--- travel-tracker-web/travel_tracker_web/urls.py
+++ travel-tracker-web/travel_tracker_web/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
@@ -6,4 +8,4 @@ urlpatterns = [
     path("", include("core.urls")),
     path("", include("userprofile.urls")),
     path("", include("app.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
