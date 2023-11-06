138bfaf5f021baabd1092c2a5ba8baa015a2bf48
Sharath RC <sharathr.sh@gmail.com>
2023-11-06 17:14:51 +0530

    make slug unique

--- travel-tracker-web/app/views.py
+++ travel-tracker-web/app/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils.text import slugify
 from django.contrib import messages
 
+import random
+import string
+
 from .models import Trip
 from .forms import TripForm
+
+
+ALPHA_NUM_LEN = 5
 
 
 def trip_detail(request, slug):
@@ -18,12 +24,17 @@ def add_new_trip(request):
         form = TripForm(request.POST, request.FILES)
 
         if form.is_valid():
+            trip_id = "".join(
+                random.choice(string.ascii_lowercase + string.digits)
+                for _ in range(ALPHA_NUM_LEN)
+            )
+
             title = request.POST.get("title")
 
             trip = form.save(commit=False)
             trip.user = request.user
 
-            slug = f"{trip.user}-{title}"
+            slug = f"{trip.user}-{trip_id}-{title}"
             trip.slug = slugify(slug)
             trip.save()
 
