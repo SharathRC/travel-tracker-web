dd9b88ae7678b3358988f8f330e97786703c60fb
Sharath RC <sharathr.sh@gmail.com>
2023-11-08 13:54:15 +0530

    add static files

--- travel-tracker-web/travel_tracker_web/settings.py
+++ travel-tracker-web/travel_tracker_web/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / STATIC_URL]
+
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / MEDIA_URL
 
